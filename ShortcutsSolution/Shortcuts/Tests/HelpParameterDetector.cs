87eed396e74a5f6fb0f5ee716a69607d3ca913e1
Jeppe Vammen Kristensen <jeppe@jeppek.dk>
2013-05-03 10:18:17 +0200

    added helpdetector and unit tests

--- ShortcutsSolution/Shortcuts/Tests/HelpParameterDetector.cs
+++ ShortcutsSolution/Shortcuts/Tests/HelpParameterDetector.cs
@@ -2,5 +2,39 @@
 
 namespace Shortcuts.Tests
 {
+    [TestFixture]
+    public class HelpParameterTester
+    {
+        [Test]
+        public void IsMatch_IsHelpMatch_ReturnsTrue()
+        {
+            var sut = CreateSubject();
+            var args = new string[0];
+            Assert.That(sut.IsMatch(args), Is.True);
+        }
+
+        private static HelpParameterDetectorAndHandlerFactory CreateSubject()
+        {
+            return new HelpParameterDetectorAndHandlerFactory();
+        }
+
+        [Test]
+        public void IsMatch_HasParameters_ReturnsFalse()
+        {
+            var sut = CreateSubject();
+            var args = new string[] {"Something"};
+            Assert.That(sut.IsMatch(args), Is.False);
+        }
+
+        [Test]
+        public void CreateProcessingInformation_IsCalled_ReturnsRelevantClass()
+        {
+            var sut = CreateSubject();
+            var args = new string[] {};
+            Assert.That(sut.CreateProcessingInformation(args), Is.TypeOf<HelpHandler>());
+        }
+
+    }
+
     
 }
