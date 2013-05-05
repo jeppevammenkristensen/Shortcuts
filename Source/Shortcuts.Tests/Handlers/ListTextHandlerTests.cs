db6319519bc2a09d8836579de0ee0c9e18e51cea
Jeppe Vammen Kristensen <jeppe@jeppek.dk>
2013-05-05 19:42:52 +0200

    Added list handler

---
+++ Source/Shortcuts.Tests/Handlers/ListTextHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using Shortcuts.Handlers;
+
+namespace Shortcuts.Tests.Handlers
+{
+    [TestFixture]
+    public class ListTextHandlerTests : HandlerTest
+    {
+         [Test]
+         public void Handle_IsHandler()
+         {
+             var handler = new ListTextHandler(new string[]{});
+             using (this.StartMockedConsole())
+             {
+                 var input = new List<string>();
+                 _consoleMock.Setup(x => x.WriteLine(It.IsAny<string>()))
+                             .Callback((string arg1, string[] arg2) => input.Add(arg1));
+                 handler.Handle();
+
+                 Assert.That(input, Has.Count.EqualTo(2));
+                 Assert.That(input, Contains.Item("Class1"));
+                 Assert.That(input, Contains.Item("Nested"));
+                
+             }
+                     
+         }
+
+    }
+}
---
+++ Source/Shortcuts/Handlers/ListTextHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Shortcuts.Handlers
+{
+    public class ListTextHandler : Handler
+    {
+        public ListTextHandler(string[] args) : base(args)
+        {
+
+        }
+
+        public override void Handle()
+        {
+            var info = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, "Files"));
+            foreach (var fileInfo in info.GetFiles("*.shortc", SearchOption.AllDirectories))
+            {
+                _Console.WriteLine(Path.GetFileNameWithoutExtension(fileInfo.FullName)); 
+            }
+        }
+    }
+}
