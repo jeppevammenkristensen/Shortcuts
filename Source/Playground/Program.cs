21fc07a294d8670fdb8943a3226f6cc97c5fb79b
Jeppe Vammen Kristensen <jeppe@jeppek.dk>
2013-05-06 21:30:40 +0200

    Updated playground

--- Source/Playground/Program.cs
+++ Source/Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace Playground
     {
         static void Main(string[] args)
         {
+            string commandLine = "dir";
+            var startInfo = new ProcessStartInfo("cmd.exe","dir")
+                {
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+
+            var process = Process.Start(startInfo);
+            using (var writer = process.StandardInput)
+            {
+                process.WaitForExit();
+                Console.WriteLine(process.StandardOutput.ReadToEnd());
+                writer.Write(commandLine);
+                
+                
+            }
+
+            Console.ReadLine();
         }
+
+
     }
 }
